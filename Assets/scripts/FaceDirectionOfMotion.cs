01543e8135aae3a4b6388e9cde7b5505e9681f51
Ian McCall <ianmccall93@gmail.com>
2018-01-26 23:48:24 -0800

    added face direction of motion

---
+++ Assets/scripts/FaceDirectionOfMotion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FaceDirectionOfMotion : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        var velocity = rigidBody.velocity;
+        var speed = velocity.magnitude;
+
+        var facingAngle = rigidBody.rotation;
+        var motionAngle = Vector2.SignedAngle(Vector2.up, velocity);
+
+        var angularDeviationRaw = (motionAngle - facingAngle) % 360;
+        var angularDeviation = angularDeviationRaw < 180 ? angularDeviationRaw : angularDeviationRaw - 360;
+
+        rigidBody.rotation += strength * Time.fixedDeltaTime * speed * angularDeviation;
+    }
+    public float strength = 1.0f;
+}
