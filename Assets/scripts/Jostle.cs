5230b0f72795a9d7692ece65ec2b5d7a0e94a33b
Ian McCall <ianmccall93@gmail.com>
2018-01-27 09:23:47 -0800

    created a Jostle component and added it to the waiters

---
+++ Assets/scripts/Jostle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Move))]
+public class Jostle : MonoBehaviour
+{
+    public float jostleAccelerationThreshold = 1.0f;
+    private Vector2 velocityPrev = Vector2.zero;
+    void FixedUpdate()
+    {
+        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        var velocityCur = rigidBody.velocity;
+        var acceleration = (velocityCur - velocityPrev) / Time.fixedDeltaTime;
+        velocityPrev = velocityCur;
+
+        var move = gameObject.GetComponent<Move>();
+        var mass = rigidBody.mass;
+        var intrinsicAcceleration = move.Force / mass;
+        var extrinsicAcceleration = acceleration - intrinsicAcceleration;
+
+        if (extrinsicAcceleration.magnitude > jostleAccelerationThreshold)
+        {
+            print(gameObject.name + " was jostled!");
+        }
+    }
+}
