32e99e6494aea133289d5e2e5a6eb7e2f3aeb34e
Ian McCall <ianmccall93@gmail.com>
2018-01-26 19:41:17 -0800

    updated player input with primitive input

--- Assets/scripts/PlayerInput.cs
+++ Assets/scripts/PlayerInput.cs
@@ -15,6 +15,17 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
 
+        var collider = gameObject.GetComponent<CircleCollider2D>();
+
+        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        rigidBody.AddForce(new Vector2(horizontal, vertical), ForceMode2D.Impulse);
+        //collider.rigidbody2D.addforce
+
+        print(vertical);
+        print(horizontal);
     }
 }
