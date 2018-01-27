2cab6f8f90fb160ea7b4d034c2e8043be3a2b281
Trent Jeffery <trentjeffery@shaw.ca>
2018-01-26 20:00:24 -0800

    Add RestaurantStateClass

---
+++ Assets/scripts/RestaurantState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestaurantState : MonoBehaviour {
+
+    enum AlertState
+    {
+        calm,
+        alert,
+        aware,
+        gtfo
+    }
+
+    AlertState alert;
+
+	// Use this for initialization
+	void Start () {
+        alert = AlertState.calm;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
