10057288237e497017364adae9669097752b5134
Ian McCall <ianmccall93@gmail.com>
2018-01-28 11:52:03 -0800

    changed alert raiser to be tiered

--- Assets/scripts/AlertRaiser.cs
+++ Assets/scripts/AlertRaiser.cs
@@ -1,8 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-[RequireComponent(typeof(CollisionDispatcher))]
+[RequireComponent(typeof(CollisionDispatcher), typeof(AudioSource))]
 public class AlertRaiser : MonoBehaviour
 {
+    [System.Serializable]
+    public struct AlertThreshold
+    {
+        public float collisionThreshold;
+        public float alertLevelIncrease;
+        public AudioClip audioClip;
+    }
+
+    public List<AlertThreshold> thresholds;
+
     public float collisionAccelerationThreshold = 1;
     public float alertLevelRaised = 0;
 
@@ -13,10 +24,41 @@ public class AlertRaiser : MonoBehaviour
         {
             cd.OnCollisionEnter += OnCollision;
         }
+
+        // Sort by descending collision threshold, so we pick most extreme first
+        thresholds.Sort((a, b) =>
+        {
+            return
+                a.collisionThreshold > b.collisionThreshold ? -1 :
+                a.collisionThreshold < b.collisionThreshold ? +1 :
+                0;
+        });
     }
 
     void OnCollision(Collision2D collision, float acceleration)
     {
+        foreach (var threshold in thresholds)
+        {
+            if (threshold.collisionThreshold < acceleration)
+            {
+                {
+                    var audioSource = GetComponent<AudioSource>();
+                    if (audioSource != null)
+                    {
+                        audioSource.PlayOneShot(threshold.audioClip, 1);
+                    }
+                }
+                {
+                    var restaurant = FindObjectOfType<RestaurantState>();
+                    if (restaurant != null)
+                    {
+                        restaurant.AddAlert(threshold.alertLevelIncrease);
+                    }
+                }
+                break;
+            }
+        }
+
         if (acceleration > collisionAccelerationThreshold
             && collision.gameObject.GetComponent<PlayerInput>() != null)
         {
