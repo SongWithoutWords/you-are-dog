c6d98675150f6516ab1aa864dbe835c8d1912fd0
Ian McCall <ianmccall93@gmail.com>
2018-01-28 13:40:22 -0800

    added stamina mechanic to player input

--- Assets/scripts/PlayerInput.cs
+++ Assets/scripts/PlayerInput.cs
@@ -1,18 +1,53 @@
 ﻿using UnityEngine;
-
-//using static MyExtensions;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Move))]
 public class PlayerInput : MonoBehaviour
 {
+    public float staminaRegenRate = 0.2f;
+    public float forceMultiplierWhileUsingStamina = 1.5f;
+    public float maxStaminaSeconds = 5.0f;
+    public Text staminaDisplayText;
+
+    private float staminaSeconds;
+
+    private void Awake()
+    {
+        staminaSeconds = maxStaminaSeconds;
+    }
+
+    float GetForceMultiplierAndUpdateStamina()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && staminaSeconds > Time.fixedDeltaTime)
+        {
+            staminaSeconds -= Time.fixedDeltaTime;
+            return forceMultiplierWhileUsingStamina;
+        }
+        else
+        {
+            staminaSeconds += staminaRegenRate * Time.fixedDeltaTime;
+            staminaSeconds = Mathf.Min(staminaSeconds, maxStaminaSeconds);
+            return 1;
+        }
+    }
+
+    void Update()
+    {
+        if (staminaDisplayText != null)
+        {
+            staminaDisplayText.text = "Stamina: " + staminaSeconds.ToString("0.0") + "/" + maxStaminaSeconds.ToString("0.0");
+        }
+    }
+
     void FixedUpdate()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
         var forceVector = new Vector2(horizontal, vertical);
 
+        var forceBoost = GetForceMultiplierAndUpdateStamina();
         var move = gameObject.GetComponent<Move>();
 
-        move.AddForce(move.forcefullness * forceVector);
+        move.AddForce(forceBoost * move.forcefullness * forceVector);
     }
 }
