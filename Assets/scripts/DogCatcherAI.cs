95da4745a8a46a8ae7b5c7848ce8d5fdbc68e0e8
John Werner <mooilo.ueu@gmail.com>
2018-01-27 17:49:10 -0800

    Initial dog catcher prefab.

---
+++ Assets/scripts/DogCatcherAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Move))]
+public class DogCatcherAI : AIBase
+{
+    void Start()
+    {
+        strategy = new PursuePlayer();
+    }
+
+    class PursuePlayer : IStrategy
+    {
+        public IStrategy Update(GameObject gameObject, AlertState alertState)
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            gameObject.GetComponent<Move>().MoveTowards(player);
+
+            return this;
+        }
+    }
+}
