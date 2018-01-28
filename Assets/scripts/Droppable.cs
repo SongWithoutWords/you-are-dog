eae8ca11bc14b11642989f5adffeaa057b79fe35
Ian McCall <ianmccall93@gmail.com>
2018-01-27 19:34:12 -0800

    droppable, dropping and dropped loop

---
+++ Assets/scripts/Droppable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine; 
+
+public class Droppable : MonoBehaviour
+{
+    public float collisionDropThreshold = 1.0f;
+    public GameObject droppingPrefab;
+
+    void Start()
+    {
+        var cd = gameObject.transform.parent.GetComponent<CollisionDispatcher>();
+        if (cd != null)
+        {
+            cd.OnCollisionEnter += OnCollision;
+        }
+    }
+
+    void OnCollision(Collision2D collision, float acceleration)
+    {
+        if (acceleration > collisionDropThreshold)
+        {
+            OnDrop(collision);
+        }
+    }
+
+    void OnDrop(Collision2D collision)
+    {
+        var dropping = Instantiate(droppingPrefab);
+        dropping.transform.position = transform.position;
+        dropping.transform.rotation = transform.rotation;
+        var rigidBody = dropping.GetComponent<Rigidbody2D>();
+        if (rigidBody != null)
+        {
+            rigidBody.velocity += collision.relativeVelocity;
+        }
+
+        var cd = gameObject.transform.parent.GetComponent<CollisionDispatcher>();
+        if (cd != null)
+        {
+            cd.OnCollisionEnter -= OnCollision;
+        }
+
+        transform.parent = null;
+        Destroy(gameObject);
+    }
+}
