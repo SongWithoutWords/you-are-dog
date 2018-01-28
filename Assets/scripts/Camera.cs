ef43f027e867a38f276486eeefff7bc3dac176c1
Trent Jeffery <trentjeffery@shaw.ca>
2018-01-27 21:08:07 -0800

    Camera follows player'

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Transform playerTransform = player.GetComponent<Transform>();
+
+        Vector3 newPosition = playerTransform.position;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+	}
+}
