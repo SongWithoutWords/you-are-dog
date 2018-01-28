338cdb1d9ab6d0a16235326e0bfe68c4308c5fca
John Werner <mooilo.ueu@gmail.com>
2018-01-27 18:27:24 -0800

    Added Restaurant Row (the main menu)

---
+++ Assets/scripts/SceneChangeTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    public string targetScene;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(targetScene, LoadSceneMode.Single);
+    }
+}
