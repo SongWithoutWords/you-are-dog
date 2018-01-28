d8c2450054c0ccadfdae491c0c3945e31a0aa7c7
Trent Jeffery <trentjeffery@shaw.ca>
2018-01-27 18:35:59 -0800

    Add PlayerEat, Food scripts and DroppedFood prefab

---
+++ Assets/scripts/PlayerEat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEat : MonoBehaviour {
+
+    int calories;
+
+	// Use this for initialization
+	void Start () {
+        calories = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Eat food on collision
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        Food food = c.gameObject.GetComponent<Food>();
+        if (food != null)
+        {
+            calories += food.calories;
+            Destroy(food.gameObject);
+        }
+    }
+}
